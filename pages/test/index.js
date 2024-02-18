b994830b472d1b1c672b10c25dbbce6ca550a94f
markusgemeinder <190774@gmx.de>
2024-02-18 20:16:30 +0100

    Hide test page

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,11 +1,8 @@
-import PresetSelect from "@/components/PresetSelect";
-
 export default function TestPage() {
   return (
     <>
       <a href="/">Home</a>
       <h2>Test Page</h2>
-      <PresetSelect />
     </>
   );
 }
