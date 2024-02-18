151b5bb1cddcb939213a4c6ba24ecd753c1ae367
markusgemeinder <190774@gmx.de>
2024-02-18 12:10:12 +0100

    Hide link to test page on index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import CardList from "@/components/CardList";
 export default function HomePage() {
   return (
     <>
-      <a href="/test">Test Page</a>
+      {/* <a href="/test">Test Page</a> */}
       <CardList />
     </>
   );
