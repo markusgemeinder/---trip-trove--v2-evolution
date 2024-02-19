08402251ca71b791602f19ce7ad0e662a10f96ef
markusgemeinder <190774@gmx.de>
2024-02-19 10:46:08 +0100

    Modify and adapt header titles

--- pages/presets/index.js
+++ pages/presets/index.js
@@ -1,3 +1,34 @@
+import {
+  ButtonContainer,
+  StyledTextButton,
+  StyledTextButtonMediumSize,
+} from "@/components/Button/TextButton";
+import {
+  MiniButtonContainer,
+  MiniButtonLabel,
+  StyledMiniButton,
+} from "@/components/Button/MiniButton";
+import {
+  TripForm,
+  StyledLabel,
+  StyledInput,
+  DateContainer,
+  PreviewContainer,
+  PreviewArea,
+  PreviewImage,
+  PackListContainer,
+  PackList,
+  TemplateContainer,
+  InputContainer,
+  ItemHeaderLabel,
+  ItemNumberContainer,
+  ItemNumberLabel,
+  ItemNameLabel,
+  ItemQuantityLabel,
+  InputItem,
+  InputQuantity,
+} from "@/components/Form/Form.styled";
+
 export default function Presets() {
   return (
     <>
