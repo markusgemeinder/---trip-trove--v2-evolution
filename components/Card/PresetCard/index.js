164db922877dfec565ca45e00817ca38ab261eb8
markusgemeinder <190774@gmx.de>
2024-02-23 21:05:53 +0100

    Update

--- components/Card/PresetCard/index.js
+++ components/Card/PresetCard/index.js
@@ -16,37 +16,37 @@ import {
 
 export default function PresetCard({ preset }) {
   return (
-    // <StyledLink href={`presets/${preset._id}`}>
-    <StyledCard>
-      <CardTitle>{preset.presetName}</CardTitle>
-      <StyledBadge>
-        <PackList2ColumnsLabelContainer>
-          <PackListLabelLeft>Item</PackListLabelLeft>
-          <PackListLabelCentered>Qty</PackListLabelCentered>
-        </PackList2ColumnsLabelContainer>
-        <PackListContainer>
-          {preset.items.map((item) => (
-            <PackList2Columns key={item._id}>
-              <StyledBadgeOnBadge>
-                <PackListItemName text={item.itemName}>
-                  {item.itemName}
-                </PackListItemName>
-              </StyledBadgeOnBadge>
-              {item.itemQuantity && (
-                <>
-                  <StyledBadgeOnBadge>
-                    <PackListItemQuantity>
-                      {item.itemQuantity}
-                    </PackListItemQuantity>
-                  </StyledBadgeOnBadge>
-                </>
-              )}
-            </PackList2Columns>
-          ))}
-        </PackListContainer>
-      </StyledBadge>
-      <CardTextCallToAction>Edit Preset</CardTextCallToAction>
-    </StyledCard>
-    // </StyledLink>
+    <StyledLink href={`presets/${preset._id}`}>
+      <StyledCard>
+        <CardTitle>{preset.presetName}</CardTitle>
+        <StyledBadge>
+          <PackList2ColumnsLabelContainer>
+            <PackListLabelLeft>Item</PackListLabelLeft>
+            <PackListLabelCentered>Qty</PackListLabelCentered>
+          </PackList2ColumnsLabelContainer>
+          <PackListContainer>
+            {preset.items.map((item) => (
+              <PackList2Columns key={item._id}>
+                <StyledBadgeOnBadge>
+                  <PackListItemName text={item.itemName}>
+                    {item.itemName}
+                  </PackListItemName>
+                </StyledBadgeOnBadge>
+                {item.itemQuantity && (
+                  <>
+                    <StyledBadgeOnBadge>
+                      <PackListItemQuantity>
+                        {item.itemQuantity}
+                      </PackListItemQuantity>
+                    </StyledBadgeOnBadge>
+                  </>
+                )}
+              </PackList2Columns>
+            ))}
+          </PackListContainer>
+        </StyledBadge>
+        <CardTextCallToAction>Edit Preset</CardTextCallToAction>
+      </StyledCard>
+    </StyledLink>
   );
 }
--- pages/presets/[id]/index.js
+++ pages/presets/[id]/index.js
@@ -1,7 +1,47 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import PresetForm from "@/components/Form/PresetForm";
+import BackButton from "@/components/Button/BackButton";
+import { toast, Toaster } from "react-hot-toast";
+
 export default function EditPreset() {
+  const router = useRouter();
+  const { isReady } = router;
+  const { id } = router.query;
+
+  const {
+    data: preset,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR(`/api/presets/${id}`);
+
+  const handleSubmit = async (presetData) => {
+    const response = await fetch(`/api/presets/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(presetData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+
+    if (!response.ok) {
+      toast.error(
+        "Oops! Something went wrong while processing your request. Please check your input and try again."
+      );
+    }
+  };
+
   return (
     <>
-      <h2>Preset Edit Page</h2>
+      <Toaster />
+      {/* <PresetForm onSubmit={handleSubmit} defaultData={trip} isEditMode={true} /> */}
+      <PresetForm />
+      <BackButton href={`/presets/${id}`} />
     </>
   );
 }
